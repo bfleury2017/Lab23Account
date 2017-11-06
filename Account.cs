984552ad5a7c4331f9e6ee114e2e85f49eacafc4
bfleury2017 <fleury.brian@gmail.com>
2017-11-06 18:33:51 -0500

    Initial

---
+++ Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Lab23Account
+{
+    public class Account
+    {
+        public int ID { get; private set; }
+
+        public string Name { get; set; }
+        private decimal balance;
+        public static int Count { get; private set; }
+
+        public Account(string name, decimal bal)
+        {
+            Name = name;
+            Balance = bal;
+            ID = ++Count;
+            Console.WriteLine($"Account created - ID: {ID}, Name: {Name}, Balance: {Balance:C}");
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            private set
+            {
+                if (value > 0.0m)
+                {
+                    balance = value;
+                }
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance = Balance + amount;
+
+            Console.WriteLine($"adding {amount} to {Name}'s balance");
+            Console.WriteLine();
+
+        }
+
+        public void Withdraw(decimal amount) {
+            if (balance >= amount)
+            {
+                Balance = Balance - amount;
+
+                Console.WriteLine($"Withdrawing {amount:C} from {Name}'s account");
+                Console.WriteLine();
+            } else
+            {
+                Console.WriteLine($"Failed, balance is: {Balance:C}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Account: ID={ID}, Name={Name}, Balance={Balance:C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab23Account
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal deposit;
+            decimal withdraw;
+
+            Account account1 = new Account("John Smith", 50);
+            Account account2 = new Account("Jackie Blue", 0);
+
+            Console.WriteLine();
+
+            Console.Write($"Enter deposit amount for {account1.Name}:");
+            deposit = decimal.Parse(Console.ReadLine());
+            account1.Deposit(deposit);
+
+            Console.Write($"Enter deposit amount for {account2.Name}:");
+            deposit = decimal.Parse(Console.ReadLine());
+            account2.Deposit(deposit);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+            Console.WriteLine();
+
+            Console.Write($"Enter withdrawal amount for {account1.Name}:");
+            withdraw = decimal.Parse(Console.ReadLine());
+            account1.Withdraw(withdraw);
+
+            Console.Write($"Enter withdrawal amount for {account2.Name}:");
+            withdraw = decimal.Parse(Console.ReadLine());
+            account2.Withdraw(withdraw);
+
+
+            Console.WriteLine(account1.ToString());
+            Console.WriteLine(account2.ToString());
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Number of accounts: {Account.Count}");
+        }
+    }
+}
